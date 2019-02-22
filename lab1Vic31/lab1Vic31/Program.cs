8d0cf30e0664313c03c6203d32ff8a7642245c99
MItnicckk <47879918+MItnicckk@users.noreply.github.com>
2019-02-22 09:33:45 +0300

    Update Program.cs

--- lab1Vic31/lab1Vic31/Program.cs
+++ lab1Vic31/lab1Vic31/Program.cs
@@ -12,10 +12,6 @@ namespace lab1Vic31
             Console.WriteLine("Мурсалимов А.Т.");
             Console.WriteLine("Семенов И.А.");
             Console.WriteLine("Абарин В.В.");
-            Console.WriteLine("Булахов А.В.");
-            Console.WriteLine("Коваленко А.В.");
-            Console.WriteLine("Гордиенко А.И.");
-            Console.WriteLine("Попов И.Ю.");
             
             Console.WriteLine("Документацией занимались: ");
             Console.WriteLine("Икаев А.С.");
@@ -23,6 +19,13 @@ namespace lab1Vic31
             Console.WriteLine("Касимов И.А.");
             Console.WriteLine("Белов И.О.");
             Console.WriteLine("Ворожев Я.А.");
+            
+            Console.WriteLine("Web-разроботкой занимались: ");
+            Console.WriteLine("Игнатенко В.A.");
+            Console.WriteLine("Рамазанов М.Р.");
+            Console.WriteLine("Луценко В.Д.");
+            Console.WriteLine("Саввин А.С.");
+            Console.WriteLine("Матвеев Д.В.");
         }
     }
 }
